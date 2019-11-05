cdc8e9885f11696aa7e519c8c356f6dc5620cd30
jlareck <medik2507@gmail.com>
2019-11-05 19:41:14 +0200

    Implemented basic classes

---
+++ SocialNetwork/src/Messages.scala
@@ -0,0 +1,13 @@
+import scala.collection.mutable.ArrayBuffer
+
+class Likes{
+  var likes: Int = 0
+  var dislikes: Int = 0
+  var rating: Int = 0
+
+
+}
+class Messages(var owner: User, var string: String, var theme: Themes,
+               var comments: ArrayBuffer[Messages], var references: ArrayBuffer[User]) {
+  var likes = new Likes()
+}
