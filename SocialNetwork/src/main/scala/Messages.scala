976d15ea9aed6f4d8babe55e972400084d2ff583
jlareck <medik2507@gmail.com>
2019-11-10 00:51:19 +0200

    added helper class for using data from mongodb

--- SocialNetwork/src/main/scala/Messages.scala
+++ SocialNetwork/src/main/scala/Messages.scala
@@ -1,10 +1,10 @@
+package scala
 import scala.collection.mutable.ArrayBuffer
 
 class Likes{
   var likes: Int = 0
   var dislikes: Int = 0
   var rating: Int = 0
-
 
 }
 class Messages(var owner: User, var string: String, var theme: Themes,
--- SocialNetwork/src/main/scala/Themes.scala
+++ SocialNetwork/src/main/scala/Themes.scala
@@ -1,3 +1,4 @@
+package scala
 class Themes {
   var theme: String = ""
 
