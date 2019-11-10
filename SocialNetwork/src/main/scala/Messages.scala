d42c48534fecb91a463caa828e5adee1f1ba6162
jlareck <medik2507@gmail.com>
2019-11-11 00:37:40 +0200

    changed structure of messages and likes classes

--- SocialNetwork/src/main/scala/Messages.scala
+++ SocialNetwork/src/main/scala/Messages.scala
@@ -1,13 +1,8 @@
 package scala
 import scala.collection.mutable.ArrayBuffer
 
-class Likes{
-  var likes: Int = 0
-  var dislikes: Int = 0
-  var rating: Int = 0
+case class Likes(var likes: Int = 0,var dislikes: Int = 0,var rating: Int = 0)
 
-}
-class Messages(var owner: String, var string: String, var theme: Themes,
-               var comments: ArrayBuffer[Messages], var references: ArrayBuffer[User]) {
-  var likes = new Likes()
-}
+
+case class Messages( owner: String,  string: String,  theme: Themes,
+               var comments: ArrayBuffer[Messages], var references: ArrayBuffer[User],  likes: Likes = Likes())
