6f9d055c109674028d389d8d3e31a38a7a329a42
jlareck <medik2507@gmail.com>
2019-11-10 23:15:11 +0200

    implemented simple registration

--- SocialNetwork/src/main/scala/Registration.scala
+++ SocialNetwork/src/main/scala/Registration.scala
@@ -1,3 +1,14 @@
-object Registration {
-  scala.io.StdIn.readLine()
+object Registration extends App {
+
+    println("Hello! Enter your username")
+    val userName: String = scala.io.StdIn.readLine()
+    println("Enter your password")
+    val password: String = scala.io.StdIn.readLine()
+    println("Enter your first name and last name")
+    val name: String = scala.io.StdIn.readLine()
+
+    val newUser = new User(userName, password, name)
+
+
+
 }
