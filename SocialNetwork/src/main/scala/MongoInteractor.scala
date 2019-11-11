6c7b62d41806645a8b03369dbaec0c392fbd92d6
jlareck <medik2507@gmail.com>
2019-11-11 20:45:27 +0200

    first version of method write message into database

--- SocialNetwork/src/main/scala/MongoInteractor.scala
+++ SocialNetwork/src/main/scala/MongoInteractor.scala
@@ -3,8 +3,11 @@ import scala.collection.mutable.ArrayBuffer
 
 import scala.Helpers._
 import org.mongodb.scala._
+import org.mongodb.scala.model.Updates._
+
 import org.mongodb.scala.model.Filters._
-import org.mongodb.scala.model._
+import org.mongodb.scala.model.ReplaceOptions
+import org.mongodb.scala.bson
 object MongoInteractor {
   val mongoClient: MongoClient = MongoClient()
 
@@ -14,18 +17,25 @@ object MongoInteractor {
 
   val collection: MongoCollection[Document] = database.getCollection("test")
 
+  def authorization(userName: String, password: String):User={
+
+  }
+
   def writeUserToDatabase(user: User): Unit ={
       val newUserDocument = Document("userName"->user.userName, "password" -> user.password, "name"-> user.name,"favouriteThemes"->Document(),
         "friends"-> Document(), "messages" -> Document(),"favouriteMessages" ->Document() )
       collection.insertOne(newUserDocument).results()
   }
-  def writeMessageToDatabase(messages: Messages): Unit ={
+  def writeMessageToDatabase(user: User,message: Messages): Unit ={
 
-    collection.updateOne(equal("userName", messages.owner),
-      combine(set("size.uom", "cm"), set("status", "P"), currentDate("lastModified"))
-    )
+    if(user.messages.isEmpty){
+        val doc = Document("text"->message.text, "owner"->message.owner,"theme"->message.theme.theme, "comments"->Document(), "references"->Document())
 
-    collection.insertOne(doc).results()
+        collection.updateOne(equal("userName", user.userName),  push("messages", doc) )
+    }
+
+
+    //collection.insertOne(doc).results()
   }
 
 
