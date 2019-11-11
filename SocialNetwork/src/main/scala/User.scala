b80a5019d9d6e69f1f49718c9e0841282a4feb77
jlareck <medik2507@gmail.com>
2019-11-11 20:49:29 +0200

    changed user class on case class

--- SocialNetwork/src/main/scala/User.scala
+++ SocialNetwork/src/main/scala/User.scala
@@ -4,27 +4,27 @@ import scala.collection.mutable.ArrayBuffer
 import scala.Helpers._
 import org.mongodb.scala._
 
-class User(val userName:String,val password: String, var name: String, val favouriteThemes: ArrayBuffer[Themes] = ArrayBuffer(), val friends: ArrayBuffer[User]= ArrayBuffer(), val messages: ArrayBuffer[Messages]= ArrayBuffer(), val favouriteMessages: ArrayBuffer[Messages]= ArrayBuffer()) {
+case class User(var id: String, userName:String, password: String, var name: String,  favouriteThemes: ArrayBuffer[Themes] = ArrayBuffer(),  friends: ArrayBuffer[User]= ArrayBuffer(), messages: ArrayBuffer[Messages]= ArrayBuffer(), favouriteMessages: ArrayBuffer[Messages]= ArrayBuffer()) {
 
 
 
 
   def createMessage (owner: String, string: String, theme: Themes,
                      comments: ArrayBuffer[Messages], references: ArrayBuffer[User] = ArrayBuffer()): ArrayBuffer[Messages] = {
-    val message = new Messages(owner, string, theme, comments, references)
+    val message =  Messages(owner, string, theme, comments, references)
     messages += message
   }
 
 
   def repost(text:String, message: Messages, references: ArrayBuffer[User]): Unit = {
-    val repostMessage = new Messages(User.this.userName, text, message.theme,ArrayBuffer[Messages](), references)
+    val repostMessage = Messages(User.this.userName, text, message.theme,ArrayBuffer[Messages](), references)
     messages+=repostMessage
   }
 
   def like(message: Messages): Unit ={
-    if(!favouriteMessages.exists(_.string == message.string)){
-      message.likes.likes += 1;
-      message.likes.rating += 1;
+    if(!favouriteMessages.exists(_.text == message.text)){
+      message.likes.likes += 1
+      message.likes.rating += 1
       favouriteMessages+=message
     }
 
@@ -35,9 +35,9 @@ class User(val userName:String,val password: String, var name: String, val favou
 
 object Test extends App{
 
-    var user1 = new User("user1","fda","a", ArrayBuffer[Themes](), ArrayBuffer[User](), ArrayBuffer[Messages](), ArrayBuffer[Messages]())
-    var theme = new Themes()
-    theme.theme = "tema"
+   // var user1 = new User("user1","fda","a", ArrayBuffer[Themes](), ArrayBuffer[User](), ArrayBuffer[Messages](), ArrayBuffer[Messages]())
+    var theme = new Themes("fasdf")
+
 //    user1.createMessage(user1., "ccccc", theme ,ArrayBuffer())
 //    user1.createMessage(user1, "bbbbb", theme ,ArrayBuffer())
 //    user1.createMessage(user1, "aaaaa", theme ,ArrayBuffer())
