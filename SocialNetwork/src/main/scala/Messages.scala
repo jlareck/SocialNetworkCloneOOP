85a287aee5eafb5dd0d49ccb376a8cc0dedcc531
jlareck <medik2507@gmail.com>
2019-11-11 19:30:40 +0200

    changed some fields

--- SocialNetwork/src/main/scala/Messages.scala
+++ SocialNetwork/src/main/scala/Messages.scala
@@ -4,5 +4,5 @@ import scala.collection.mutable.ArrayBuffer
 case class Likes(var likes: Int = 0,var dislikes: Int = 0,var rating: Int = 0)
 
 
-case class Messages( owner: String,  string: String,  theme: Themes,
-               var comments: ArrayBuffer[Messages], var references: ArrayBuffer[User],  likes: Likes = Likes())
+case class Messages(owner: String, text: String, theme: Themes,
+                    var comments: ArrayBuffer[Messages]= ArrayBuffer(), var references: ArrayBuffer[User]=ArrayBuffer(), likes: Likes = Likes())
