cb9c1e9b3d13c522150f68d30ee004e626d16033
jlareck <medik2507@gmail.com>
2019-11-13 18:54:37 +0200

    implemented parser for Messages and writing message to database

--- SocialNetwork/src/main/scala/MongoInteractor.scala
+++ SocialNetwork/src/main/scala/MongoInteractor.scala
@@ -5,7 +5,7 @@ import io.circe._
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
-
+import org.bson.types.ObjectId
 import scala.Helpers._
 import org.mongodb.scala._
 import org.mongodb.scala.model.Updates._
@@ -24,7 +24,7 @@ object MongoInteractor {
   implicit val userDecoder: Decoder[User] =
     (hCursor: HCursor) => {
       for {
-        id <- hCursor.get[ObjectId]("_id")
+        id <- hCursor.get[String]("_id")
         userName <- hCursor.get[String]("userName")
         password <- hCursor.get[String]("password")
         name <- hCursor.get[String]("name")
@@ -32,28 +32,42 @@ object MongoInteractor {
         messages <- hCursor.get[Array[Messages]]("messages")
         friends <- hCursor.get[Array[User]]("friends")
         favouriteMessages <- hCursor.get[Array[Messages]]("favouriteMessages")
-      } yield User(id.toString,userName,password, name, favourireThemes, friends, messages, favouriteMessages)
+      } yield User(id,userName,password, name, favourireThemes, friends, messages, favouriteMessages)
     }
+  implicit val messageDecoder: Decoder[Messages] =
+    (hCursor: HCursor) => {
+      for {
+        text <- hCursor.get[String]("text")
+        owner <- hCursor.get[String]("owner")
+        theme <- hCursor.get[String]("theme")
+        comments <- hCursor.get[Array[Messages]]("comments")
+        references <- hCursor.get[Array[User]]("references")
+        likes <- hCursor.get[Likes]("likes")
 
-  def authorization(userName: String, password: String):Unit ={
-      val a = collection.find(and(equal("userName",userName),equal("password", password))).convertToJsonString().stripMargin
-      val b = parser.decode[User](a)
-      println(b)
-
+      } yield Messages(text,owner, Themes(theme), comments,references,likes)
+    }
+  def authorization(userName: String, password: String):User ={
+      val foundUser = collection.find(and(equal("userName",userName),equal("password", password))).convertToJsonString().stripMargin
+      val decodedUser = parser.decode[User](foundUser).toOption.get
+      decodedUser
   }
 
   def writeUserToDatabase(user: User): Unit ={
-      val newUserDocument = BsonDocument("userName"->user.userName, "password" -> user.password, "name"-> user.name,"favouriteThemes"-> BsonArray(),
+      val newUserDocument = BsonDocument("_id"->user.id, "userName"->user.userName,
+        "password" -> user.password, "name"-> user.name,"favouriteThemes"-> BsonArray(),
         "friends"-> BsonArray(), "messages" -> BsonArray(),"favouriteMessages" ->BsonArray() )
       collection.insertOne(newUserDocument).results()
   }
-  def writeMessageToDatabase(user: User,message: Messages): Unit ={
+  def writeMessageToDatabase(message: Messages, path:String): Unit ={
+    val doc = BsonDocument("text"->message.text, "owner"->message.owner,"theme"->message.theme.theme, "comments"->BsonArray(),
+      "references"->BsonArray(),"likes"->BsonDocument("likes"->message.likes.likes,"dislikes"->message.likes.dislikes,"rating"->message.likes.rating))
 
-    if(user.messages.isEmpty){
-        val doc = BsonDocument("text"->message.text, "owner"->message.owner,"theme"->message.theme.theme, "comments"->BsonDocument(), "references"->BsonDocument())
-
-        collection.updateOne(equal("userName", user.userName),  push("messages", doc) )
-    }
+//    if(user.messages.isEmpty){
+//
+//        collection.updateOne(equal("userName", user.userName),  push("messages", doc) )
+//    }
+//    else{
+    collection.updateOne(equal("userName", message.owner), push(path, doc)).results()
 
 
     //collection.insertOne(doc).results()
