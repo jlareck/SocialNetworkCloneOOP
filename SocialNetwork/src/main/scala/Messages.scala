a4aded660c51310be8031516e08d1bf0b1ec947b
jlareck <medik2507@gmail.com>
2019-11-17 23:55:17 +0200

    added comment method and added some new fields

--- SocialNetwork/src/main/scala/Messages.scala
+++ SocialNetwork/src/main/scala/Messages.scala
@@ -1,4 +1,8 @@
 //package scala
+// TODO: Think about how to delete message
+// TODO: Implement adding liked and disliked messages to arrays
+import org.mongodb.scala.bson.{BsonArray, BsonDocument}
+
 import scala.collection.mutable.ArrayBuffer
 
 case class Likes(var likes: Int = 0,var dislikes: Int = 0,var rating: Int = 0)
@@ -6,7 +10,19 @@ case class Likes(var likes: Int = 0,var dislikes: Int = 0,var rating: Int = 0)
 
 case class Messages( text: String,owner: String, theme: Themes,
                     var comments: ArrayBuffer[Messages] = ArrayBuffer(),
-                    var references: ArrayBuffer[User] = ArrayBuffer(), likes: Likes = Likes()){
+                    var references: ArrayBuffer[User] = ArrayBuffer(),
+                     likes: Likes = Likes(), usersWhoLiked: ArrayBuffer[String]= ArrayBuffer(), usersWhoDisliked: ArrayBuffer[String]= ArrayBuffer()){
 
+  def comment(text: String, path:String,userNameToComment: String, theme: Themes, references: ArrayBuffer[User] ): Unit ={
+    val message =  Messages(text, owner, theme, ArrayBuffer(), references)
+    comments+=message
+    MongoInteractor.writeMessageToDatabase(message, path, userNameToComment)
+  }
+
+  //  def like(path: String, ownerOfMessage:String): Unit ={
+//    if(!favouriteMessages.exists(x=> x.path == path && x.ownerOfMessage == ownerOfMessage )){
+//      MongoInteractor.likeMessage(userName, ownerOfMessage,path)
+//    }
+//  }
 }
 
