52813abef83b36a3fbfb9ce350dbe48e0df7f9f4
jlareck <medik2507@gmail.com>
2019-11-19 02:02:37 +0200

    some changes

--- SocialNetwork/src/main/scala/Registration.scala
+++ SocialNetwork/src/main/scala/Registration.scala
@@ -9,10 +9,17 @@ object Registration extends App {
     println("Enter your first name and last name")
     val name: String = scala.io.StdIn.readLine()
 
-    val newUser = new User("",userName, password, name)
-    newUser.id = System.identityHashCode(newUser).toString
+    val newUser =  User("",userName, password, name)
+    newUser.id = userName
     println(newUser.id)
     MongoInteractor.writeUserToDatabase(newUser)
 
 
+}
+object RegistrationTest extends App{
+    val n = 10
+    for (i <- 0 until n) {
+        MongoInteractor.writeUserToDatabase(User("user"+i, "user"+i, "p"+i, "USER"+i)
+        )
+    }
 }
