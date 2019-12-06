57c0dfd80ce1f36b5b9b4303d2ac44e90c35550b
jlareck <medik2507@gmail.com>
2019-12-06 13:53:07 +0200

    moved up SimpleTest for better using

--- SocialNetwork/src/main/scala/Test.scala
+++ SocialNetwork/src/main/scala/Test.scala
@@ -5,13 +5,76 @@ import org.mongodb.scala.model.Aggregates
 
 import scala.collection.mutable.ArrayBuffer
 import Helpers._
-import org.mongodb.scala.model.changestream.ChangeStreamDocument
+
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
+object SimpleTest extends App{
+  def user1Execution():Unit={
+    val user1 = MongoInteractor.authorization("user1","p1")
+    user1.subscribeOnUser("user2")
+    Thread.sleep(1000)
+    for (i<-0 until 2){
+      user1.createMessage("Hello world", Themes("111" ))
+      Test.printInfo(i.toString + " "+"user1")
+      Thread.sleep(1000)
+    }
+  }
+  def user2Execution():Unit={
+    val user2 = MongoInteractor.authorization("user2","p2")
+    user2.subscribeOnUser("user1")
+    val pipeline = ArrayBuffer(Aggregates.filter(Document("{'fullDocument.userName': 'user2'}")))
+    val observable: ChangeStreamObservable[Document] = collectionTest.watch(pipeline).fullDocument(FullDocument.UPDATE_LOOKUP)
+
+    observable.subscribe(observer)
+    Thread.sleep(1000)
+    user2.repost(Path( "user1","messages.0"), ArrayBuffer("user1"))
+    user2.like(Path( "user1","messages.0"))
+    for (i<-0 until 1){
+      user2.createMessage("AAAAAAAAAAAAA", Themes("111" ))
+      Test.printInfo(i.toString + " "+"user2")
+      Thread.sleep(1000)
+    }
+  }
+  def user3Execution():Unit={
+    val user3 = MongoInteractor.authorization("user3","p3")
+    user3.subscribeOnUser("user2")
+    val pipeline = ArrayBuffer(Aggregates.filter(Document("{'fullDocument.userName': 'user3'}")))
+    val observable: ChangeStreamObservable[Document] = collectionTest.watch(pipeline).fullDocument(FullDocument.UPDATE_LOOKUP)
+
+    observable.subscribe(observer)
+    Thread.sleep(1000)
+    user3.comment("WOW", "messages.0", "user2",Themes("aaaaaa"))
+    user3.createMessage("fasdf",Themes("f111"),ArrayBuffer("user1"))
+    for (i<-0 until 2){
+      user3.createMessage("Hello "+i, Themes("111" ))
+      Test.printInfo(i.toString + " "+"user3")
+      Thread.sleep(1000)
+    }
+  }
+  def runTask: Seq[Future[Unit]] = {
+    Seq (
+      Future {
+        user1Execution()
+      }, Future {
+        user2Execution()
+      }, Future{
+        user3Execution()
+      }
+
+
+
+
+    )
+  }
+  val task = Future.sequence(runTask)
+  Await.result(task, Duration.Inf)
+  // val user3 = MongoInteractor.authorization("user3","p3")
+  //user3.printTimeline()
+}
 
 object Test extends App{
 
@@ -99,66 +162,3 @@ object Test extends App{
       //user1.printTimeline()
 }
 
-object SimpleTest extends App{
-  def user1Execution():Unit={
-    val user1 = MongoInteractor.authorization("user1","p1")
-    user1.subscribeOnUser("user2")
-    Thread.sleep(1000)
-    for (i<-0 until 2){
-      user1.createMessage("Hello world", Themes("111" ))
-      Test.printInfo(i.toString + " "+"user1")
-      Thread.sleep(1000)
-    }
-  }
-  def user2Execution():Unit={
-    val user2 = MongoInteractor.authorization("user2","p2")
-    user2.subscribeOnUser("user1")
-    val pipeline = ArrayBuffer(Aggregates.filter(Document("{'fullDocument.userName': 'user2'}")))
-    val observable: ChangeStreamObservable[Document] = collectionTest.watch(pipeline).fullDocument(FullDocument.UPDATE_LOOKUP)
-
-    observable.subscribe(observer)
-    Thread.sleep(1000)
-    user2.repost(Path( "user1","messages.0"), ArrayBuffer("user1"))
-    user2.like(Path( "user1","messages.0"))
-    for (i<-0 until 1){
-      user2.createMessage("AAAAAAAAAAAAA", Themes("111" ))
-      Test.printInfo(i.toString + " "+"user2")
-      Thread.sleep(1000)
-    }
-  }
-  def user3Execution():Unit={
-    val user3 = MongoInteractor.authorization("user3","p3")
-    user3.subscribeOnUser("user2")
-    val pipeline = ArrayBuffer(Aggregates.filter(Document("{'fullDocument.userName': 'user3'}")))
-    val observable: ChangeStreamObservable[Document] = collectionTest.watch(pipeline).fullDocument(FullDocument.UPDATE_LOOKUP)
-
-    observable.subscribe(observer)
-    Thread.sleep(1000)
-    user3.comment("WOW", "messages.0", "user2",Themes("aaaaaa"))
-    user3.createMessage("fasdf",Themes("f111"),ArrayBuffer("user1"))
-    for (i<-0 until 2){
-      user3.createMessage("Hello "+i, Themes("111" ))
-      Test.printInfo(i.toString + " "+"user3")
-      Thread.sleep(1000)
-    }
-  }
-  def runTask: Seq[Future[Unit]] = {
-    Seq (
-      Future {
-        user1Execution()
-      }, Future {
-        user2Execution()
-      }, Future{
-        user3Execution()
-      }
-
-
-
-
-    )
-  }
-  val task = Future.sequence(runTask)
-  Await.result(task, Duration.Inf)
- // val user3 = MongoInteractor.authorization("user3","p3")
-  //user3.printTimeline()
-}
